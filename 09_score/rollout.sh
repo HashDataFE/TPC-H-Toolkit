9261c24225a08da215b9fa48042d2fc29923afc6
RyanWei <rainmanwy@gmail.com>
2025-07-02 22:43:46 +0800

    debug

--- 09_score/rollout.sh
+++ 09_score/rollout.sh
@@ -10,11 +10,14 @@ printf "\n"
 
 init_log ${step}
 
-LOAD_TIME=$(psql -v ON_ERROR_STOP=1 -q -t -A -c "select round(sum(extract('epoch' from duration))) from tpch_reports.load where tuples > 0")
-ANALYZE_TIME=$(psql -v ON_ERROR_STOP=1 -q -t -A -c "select round(sum(extract('epoch' from duration))) from tpch_reports.sql where tuples = -1")
-QUERIES_TIME=$(psql -v ON_ERROR_STOP=1 -q -t -A -c "select round(sum(extract('epoch' from duration))) from (SELECT split_part(description, '.', 2) AS id, min(duration) AS duration FROM tpch_reports.sql where tuples >= 0 GROUP BY split_part(description, '.', 2)) as sub")
-CONCURRENT_QUERY_TIME=$(psql -v ON_ERROR_STOP=1 -q -t -A -c "select round(sum(extract('epoch' from duration))) from tpch_testing.sql")
-THROUGHPUT_ELAPSED_TIME=$(psql -v ON_ERROR_STOP=1 -q -t -A -c "select max(end_epoch_seconds) - min(start_epoch_seconds) from tpch_testing.sql")
+LOAD_TIME=$(psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -q -t -A -c "SELECT ROUND(MAX(end_epoch_seconds) - MIN(start_epoch_seconds)) FROM tpch_reports.load WHERE tuples > 0")
+ANALYZE_TIME=$(psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -q -t -A -c "select round(sum(extract('epoch' from duration))) from tpch_reports.sql where id = 1")
+QUERIES_TIME=$(psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -q -t -A -c "select round(sum(extract('epoch' from duration))) from (SELECT split_part(description, '.', 2) AS id, min(duration) AS duration FROM tpch_reports.sql where tuples >= 0 GROUP BY split_part(description, '.', 2)) as sub")
+CONCURRENT_QUERY_TIME=$(psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -q -t -A -c "select round(sum(extract('epoch' from duration))) from tpcds_testing.sql")
+THROUGHPUT_ELAPSED_TIME=$(psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -q -t -A -c "select max(end_epoch_seconds) - min(start_epoch_seconds) from tpcds_testing.sql")
+SUCCESS_QUERY=$(psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -q -t -A -c "select count(*) from tpch_reports.sql where tuples >= 0")
+FAILD_QUERY=$(psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -q -t -A -c "select count(*) from tpch_reports.sql where tuples < 0 and id > 1")
+
 
 S_Q=${MULTI_USER_COUNT}
 SF=${GEN_DATA_SCALE}
@@ -41,20 +44,22 @@ QPHH=$(psql -v ON_ERROR_STOP=1 -q -t -A -c "select sqrt(cast(${POWER} as decimal
 # Note: TOTAL_PRICE should be set as an environment variable with system cost
 PRICE_PER_KQPHH=$(psql -v ON_ERROR_STOP=1 -q -t -A -c "select 1000 * cast(${TOTAL_PRICE} as decimal) / cast(${QPHH} as decimal)")
 
+printf "Number of Streams (Sq)\t%d\n" "${S_Q}"
+printf "Scale Factor (SF)\t%d\n" "${SF}"
+printf "Load (seconds)\t\t\t%d\n" "${LOAD_TIME}"
+printf "Analyze (seconds)\t\t\t%d\n" "${ANALYZE_TIME}"
+printf "1 User Queries (seconds)\t\t%d\tFor %d success queries and %d failed queries\n" "${QUERIES_TIME}" "${SUCCESS_QUERY}" "${FAILD_QUERY}"
+printf "Sum of Elapse Time for all Concurrent Queries (seconds)\t%d\n" "${CONCURRENT_QUERY_TIME}"
+printf "Throughput Test Elapsed Time (seconds)\t%d\n" "${THROUGHPUT_ELAPSED_TIME}"
+printf "\n"
+
+
 printf "TPC-H v3.0.1 Performance Metrics\n"
 printf "====================================\n"
-printf "Power@%dGB\t\t\t%.1f QphH\n" ${SF} ${POWER}
-printf "Throughput@%dGB\t\t\t%.1f QphH\n" ${SF} ${THROUGHPUT}
-printf "QphH@%dGB\t\t\t%.1f QphH\n" ${SF} ${QPHH}
-printf "Price/kQphH@%dGB\t\t\t$%.2f\n" ${SF} ${PRICE_PER_KQPHH}
-printf "\n"
-printf "Number of Streams (Sq)\t\t\t%d\n" "${S_Q}"
-printf "Scale Factor (SF)\t\t\t%d\n" "${SF}"
-printf "Load\t\t\t%d\n" "${LOAD_TIME}"
-printf "Analyze\t\t\t%d\n" "${ANALYZE_TIME}"
-printf "1 User Queries\t\t\t%d\n" "${QUERIES_TIME}"
-printf "Concurrent Queries\t\t\t%d\n" "${CONCURRENT_QUERY_TIME}"
-printf "Throughput Test Elapsed Time\t\t\t%d\n" "${THROUGHPUT_ELAPSED_TIME}"
+printf "Power@%dGB\t\t%.1f QphH\n" ${SF} ${POWER}
+printf "Throughput@%dGB\t\t%.1f QphH\n" ${SF} ${THROUGHPUT}
+printf "QphH@%dGB\t\t%.1f QphH\n" ${SF} ${QPHH}
+printf "Price/kQphH@%dGB\t\t$%.2f\n" ${SF} ${PRICE_PER_KQPHH}
 printf "\n"
 
 echo "Finished ${step}"
