68d68284cf5a5ff65d51d9b1a178ccbdabb1ae06
Ryan Wei <rainmanwy@gmail.com>
2025-07-04 18:50:24 +0800

    debug

--- 06_single_user_reports/analyze_report.sql
+++ 06_single_user_reports/analyze_report.sql
@@ -1,4 +1,4 @@
 SELECT split_part(description, '.', 1) as schema_name, round(extract('epoch' from duration)) AS seconds 
-FROM :report_schema.sql 
+FROM :report_schema.load
 WHERE id = 1
 ORDER BY 1;
