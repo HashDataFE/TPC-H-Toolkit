f80f4e126bf386e745da690985418fdf4b1e5e01
RyanWei <rainmanwy@gmail.com>
2025-07-08 22:30:16 +0800

    debug

--- tpch.sh
+++ tpch.sh
@@ -15,6 +15,8 @@ export TPC_H_DIR
 log_time "TPC-H test started"
 printf "\n"
 
+log_time "TPC-H toolkit version is: V1.0"
+
 # Check that pertinent variables are set in the variable file.
 check_variables
 # Make sure this is being run as gpadmin
@@ -27,6 +29,10 @@ export DB_VERSION=${VERSION}
 export DB_VERSION_FULL=${VERSION_FULL}
 log_time "Current database is:\n${DB_VERSION}"
 log_time "Current database version is:\n${DB_VERSION_FULL}"
+
+if [ "${DB_VERSION}" == "postgresql" ]; then
+  export RUN_MODEL="cloud"
+fi
 
 if [ "${RUN_MODEL}" != "cloud" ]; then
   source_bashrc
