a45a1aed56effb9f2eacd6408b656773a474fc27
Ryan Wei <rainmanwy@gmail.com>
2025-08-06 11:09:36 +0800

    update version to 1.2.1

--- tpch.sh
+++ tpch.sh
@@ -15,7 +15,7 @@ export TPC_H_DIR
 log_time "TPC-H test started"
 printf "\n"
 
-log_time "TPC-H toolkit version is: V1.2_dev20250806"
+log_time "TPC-H toolkit version is: V1.2.1"
 
 # Check that pertinent variables are set in the variable file.
 check_variables
